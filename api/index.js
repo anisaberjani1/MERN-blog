83e0e8c50fbf56f41c866c1524f765d58599cfa8
anisaberjani1 <anisa.berjani@gmail.com>
2024-06-08 22:06:28 +0200

    signup api route creation and small change to header

--- api/index.js
+++ api/index.js
@@ -2,17 +2,21 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
+mongoose.connect(process.env.MONGO,)
     .then(
         () => {console.log('MongoDB is connected')}
+        
     ).catch((err) => {
-        console.log(err)
+        console.error('Error connecting to MongoDB:', err);
     })
 
 const app = express();
+
+app.use(express.json())
 
 
 app.listen(3000, () => {
@@ -20,3 +24,4 @@ app.listen(3000, () => {
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,9 @@
 //import React from 'react'
 
 import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation} from "react-router-dom";
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon} from 'react-icons/fa'
-import { useLocation } from "react-router-dom";
 
 export default function Header() {
     const path = useLocation().pathname;
@@ -12,9 +11,8 @@ export default function Header() {
   return (
     <Navbar className="border-b-2">
         <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-400
-             via-purple-500 to-pink-500 rounded-lg text-white">Anisa s </span>
-            Blog
+            <span className="px-2 py-1 bg-teal-400 rounded-lg text-white">Code</span>
+            Chronicles
         </Link>
         <form>
             <TextInput 
@@ -24,20 +22,23 @@ export default function Header() {
                 className="hidden lg:inline"
             />
         </form>
+
         <Button className="w-12 h-10 lg:hidden" color="gray" pill>
             <AiOutlineSearch/>
         </Button>
+
         <div className="flex gap-2 md:order-2">
             <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
                 <FaMoon />
             </Button>
             <Link to='/sign-in'>
-                <Button gradientDuoTone='purpleToBlue' outline>
+                <Button gradientDuoTone="cyanToBlue" outline>
                     Sign In
                 </Button>
             </Link>
             <Navbar.Toggle/>
         </div>
+
         <Navbar.Collapse>
             <Navbar.Link active={path === '/'} as={'div'}>
                 <Link to='/'>Home</Link>
