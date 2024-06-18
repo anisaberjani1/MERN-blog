3073ba621d916c48b7ac46d2363a321d7f1f04f2
anisaberjani1 <anisa.berjani@gmail.com>
2024-06-18 23:09:39 +0200

    Connecting to PostgreSQL database

--- api/index.js
+++ api/index.js
@@ -3,10 +3,14 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+//import postRoutes from './routes/post.route.js'
 import cookieParser from 'cookie-parser'
+import { Sequelize } from 'sequelize'
 
+//Environment variables .env
 dotenv.config();
 
+//MongoDb connection
 mongoose.connect(process.env.MONGO,)
     .then(
         () => {console.log('MongoDB is connected')}
@@ -14,6 +18,22 @@ mongoose.connect(process.env.MONGO,)
     ).catch((err) => {
         console.error('Error connecting to MongoDB:', err);
     })
+
+//Sequelize setup for postgreSQL
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+})
+
+//Testing the connection
+sequelize.authenticate()
+    .then(() => {
+        console.log('PostgreSQL database connected');
+    })
+    .catch(err => {
+        console.log('Unable to connect to the database: ', err)
+    });
 
 const app = express();
 
@@ -26,6 +46,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
+//app.use('/api/post', postRoutes)
 
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500;
@@ -36,3 +57,5 @@ app.use((err,req,res,next) => {
         message
     })
 });
+
+export default sequelize;
