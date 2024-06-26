fc5f906c5945a2402476714a8a5dbb1e4d04d6f0
anisaberjani1 <anisa.berjani@gmail.com>
2024-06-26 21:47:10 +0200

    About and Projects pages completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,25 @@
-//import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">About Code Chronicles</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Code Chronicles! On this blog, you will find weekly articles
+              and tutorials on topics such as web development, 
+              software engineering, and programming languages.
+            </p>
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other peoples comments and reply to them
+              as well. We believe that a community of learners can help each 
+              other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
